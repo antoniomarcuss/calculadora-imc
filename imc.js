25290be7680fd951cf698b928eed327e420c5fe6
antoniomarcuss <105389155+antoniomarcuss@users.noreply.github.com>
2023-04-17 15:25:54 -0300

    add

--- imc.js
+++ imc.js
@@ -8,15 +8,15 @@ let btn = document.getElementById('btn')
                 let imc = (peso / (altura * altura)).toFixed(1)
                 let classificacao = ''
                 if(imc < 18.5){
-                    classificacao = 'ABAIXO DO PESO!'
+                    classificacao = 'abaixo do peso!'
                 }else if(imc >= 18.5 && imc <= 24.9){
-                    classificacao = 'com o peso o NORMAL!'
+                    classificacao = 'com o peso o normal!'
                 }else if(imc >= 25 && imc <= 29.9){
-                    classificacao = 'com SOBREPESO!'
+                    classificacao = 'com sobrepeso!'
                 }else if(imc >= 30 && imc <= 39.9){
-                    classificacao = 'com OBESIDADE GRAU II!'
+                    classificacao = 'com obesidade grau II!'
                 }else if(imc > 40){
-                    classificacao = 'com OBESIDADE GRAVE GRAU III! <br> CUIDADO!'                 
+                    classificacao = 'com obesidade grau III! <br> CUIDADO!'                 
                 }
                 res.innerHTML = `${nome} seu IMC é ${imc} e você está ${classificacao}`
 
@@ -24,3 +24,5 @@ let btn = document.getElementById('btn')
                 alert('Preencha todos os Campos!')}
             e.preventDefault()
         })
+
+       
